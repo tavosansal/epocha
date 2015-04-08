d089d819722b0bdf28cd610b3fbbec6dee056814
Gustavo San Jose <gustavo@todaymade.com>
2015-04-08 17:00:20 -0500

    [FEATURE] Added real time timestamp and local date and time

--- app/controllers/robot.js
+++ app/controllers/robot.js
@@ -1,17 +1,34 @@
 import Ember from 'ember';
 /* global moment */
+/* global $ */
 
 export default Ember.Controller.extend({
-    setValue: function() {
-        var currentTimestamp = moment().unix();
-        this.set('value', currentTimestamp);
+    updateTime: function() {
+        var _this = this;
+
+        // Update the time every second.
+        var updateClock = Ember.run.later(function() {
+            _this.set('currentTime', moment().unix());
+            _this.updateTime();
+        }, 1000);
+        this.set('updateClock', updateClock);
     }.on('init'),
 
+    currentTime: moment().unix(),
+    isNotPaused: true,
+
+    humanDate: function() {
+        return moment.unix(this.get('currentTime'));
+    }.property('currentTime'),
+
     actions: {
-        getDate: function() {
-            var robotDate = this.get('value');
-            var humanDate = moment.unix(robotDate);
-            this.set('humanDate', humanDate);
+        pause: function() {
+            Ember.run.cancel(this.get('updateClock'));
+            this.set('isNotPaused', false);
+        },
+        play: function() {
+            this.set('isNotPaused', true);
+            this.updateTime();
         }
     }
 });
