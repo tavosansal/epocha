7ecbdb63f521f75f230c2f807e5f6a2198d2eb1c
Gustavo San Jose <gustavo@coschedule.com>
2016-12-13 11:59:18 -0600

    [CHORE] More updates and code cleanup

--- app/services/current-time.js
+++ app/services/current-time.js
@@ -5,59 +5,61 @@ import _ from 'lodash/lodash';
 export default Ember.Service.extend({
 
   init() {
-      this._super(...arguments);
-      this.setupTimezones();
+    this._super(...arguments);
+    this.setupTimezones();
+    this.updateTime();
+  },
+
+  updateTime() {
+    // Update the time every second.
+    const updateClock = Ember.run.later(() => {
+      if (!this.get('isDestroyed') && !this.get('isDestroying')) {
+        this.set('currentTime', moment().unix());
+      }
       this.updateTime();
-    },
+    }, 1000);
+    this.set('updateClock', updateClock);
+  },
 
-    updateTime: function() {
-      var _this = this;
+  setupTimezones() {
+    let timeZonesList = _.map(moment.tz.names(), (zone) => {
+      let group = zone.split('/');
 
-      // Update the time every second.
-      var updateClock = Ember.run.later(function() {
-        _this.set('currentTime', moment().unix());
-        _this.updateTime();
-      }, 1000);
-      this.set('updateClock', updateClock);
-    },
+      if (group.length > 1) {
+        group = group[0];
+      } else {
+        group = 'Other';
+      }
 
-    setupTimezones: function() {
-      let timeZonesList = _.map(moment.tz.names(), function(zone) {
-        var group = zone.split('/');
-
-        if (group.length > 1) {
-          group = group[0];
-        } else {
-          group = 'Other';
-        }
-
-        return {
-          name: zone,
-          group: group
-        };
-      });
-
-      let groupedZones = _.groupBy(timeZonesList, 'group');
-
-      let formattedGroups = [];
-
-      _.forEach(groupedZones, function(group, key) {
-        formattedGroups.push({
-          groupName: key,
-          options: group
-        });
-      });
-
-      this.set('timezones', formattedGroups);
-
-      //Find user timezone
-      let userTz = moment.tz.guess();
-      userTz = {
-        name: userTz,
-        group: userTz.split('/').length > 1 ? userTz.split('/')[0] : 'Other'
+      return {
+        name: zone,
+        group: group
       };
+    });
 
+    let groupedZones = _.groupBy(timeZonesList, 'group');
+
+    let formattedGroups = [];
+
+    _.forEach(groupedZones, (group, key) => {
+      formattedGroups.push({
+        groupName: key,
+        options: group
+      });
+    });
+
+    this.set('timezones', formattedGroups);
+
+    //Find user timezone
+    let userTz = moment.tz.guess();
+    userTz = {
+      name: userTz,
+      group: userTz.split('/').length > 1 ? userTz.split('/')[0] : 'Other'
+    };
+
+    if (!this.get('isDestroyed') && !this.get('isDestroying')) {
       this.set('currentTimezone', userTz);
-    },
+    }
+  },
 
 });
