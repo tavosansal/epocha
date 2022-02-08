6c2a3254e360a788ec8956fe77f52ecd386187c6
tavosansal <gustavoasanjose@gmail.com>
2022-02-08 00:03:31 -0600

    [CHORE] Adds manifest.js

---
+++ config/manifest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "epocha",
+    short_name: "epocha",
+    description: "",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
