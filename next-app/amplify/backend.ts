d92e2248b1cdab5287ab9245ef6de813512c67be
Gustavo San Jose <gustavoasanjose@gmail.com>
2025-12-23 18:57:37 -0600

    chore: changes

---
+++ next-app/amplify/backend.ts
@@ -0,0 +1,8 @@
+import { defineBackend } from '@aws-amplify/backend';
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({
+  // Add your backend resources here
+});
